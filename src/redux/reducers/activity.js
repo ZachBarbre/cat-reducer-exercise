fe260a8132bdb4db9626751ab388a33824f11cd4
Zach Barbre <zach.barbre@gmail.com>
2020-04-07 11:20:54 -0400

    add action for setName. refactor activities to use setActivity

--- src/redux/reducers/activity.js
+++ src/redux/reducers/activity.js
@@ -1,26 +1,25 @@
 // import action types
-import { ACTION_NAP, ACTION_EAT, ACTION_PLAY } from '../actionTypes';
+// import { ACTION_NAP, ACTION_EAT, ACTION_PLAY } from '../actionTypes';
+import { ACTION_SET_NAME, ACTION_SET_ACTIVITY } from '../actionTypes';
 
 const initalState = {
+    name: 'Guster',
     activity: 'napping'
 }
 
 const activityReducer = (state = initalState, action) => {
     switch(action.type) {
-        case ACTION_NAP: 
+        case ACTION_SET_NAME: 
+            const { name } = action.payload;
             return { 
-                ...state,
-                activity: 'napping' 
+                name: name,
+                ...state 
             }
-        case ACTION_EAT: 
+        case ACTION_SET_ACTIVITY: 
+            const { activity } = action.payload
             return { 
                 ...state,
-                activity: 'eating' 
-            }
-        case ACTION_PLAY: 
-            return { 
-                ...state,
-                activity: 'playing' 
+                activity: activity
             }
         default:
             return state;
